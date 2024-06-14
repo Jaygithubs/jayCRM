d2180640df1da768bcc141331281f97eb82c9611
Jaygithubs <jaysharma89533@gmail.com>
2024-06-13 21:26:05 -1200

    try to connect db

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,18 @@
-const connectToMongo = require('./db');
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
-connectToMongo();
+const app=express();
+dotenv.config();
+
+
+const PORT = process.env.PORT || 7000;
+const MONGOURL= process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL).then(()=>{
+    console.log("Database connected successfully.");
+    app.listen(PORT,() =>{
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((error)=>{console.log(error)})
+
