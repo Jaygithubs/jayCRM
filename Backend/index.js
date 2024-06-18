bc4a4443d559bcb83aae3725d5668da2a8b2f848
Jaygithubs <jaysharma89533@gmail.com>
2024-06-17 21:27:19 -1200

    connect db

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,28 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-
-const app=express();
-dotenv.config();
+// import express from "express"
+// import mongoose from "mongoose"
+// import dotenv from "dotenv"
 
 
-const PORT = process.env.PORT || 7000;
-const MONGOURL= process.env.MONGO_URL;
+// const app=express();
+// dotenv.config();
 
-mongoose.connect(MONGOURL).then(()=>{
-    console.log("Database connected successfully.");
-    app.listen(PORT,() =>{
-        console.log(`Server is running on port ${PORT}`);
-    });
-}).catch((error)=>{console.log(error)})
+// const PORT = process.env.PORT || 7000;
+// const MONGOURL = process.env.MONGO_URL;
 
+// mongoose.connect("MONGOURL").then(()=>{
+//     console.log("Database connected succesfully");
+//     app.listen(PORT,()=>{
+//         console.log(`server is running on ${PORT}`);
+//     });
+// }).catch((error)=>console.log(error));
+
+const mongoose = require('mongoose');
+
+const uri = 'mongodb+srv://jaysharma89533:%23jayDB@jaymongodb.fdlxsvh.mongodb.net/';
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('MongoDB connection error:', err));
