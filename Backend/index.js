4bd50edd79433a8a0e8f8e9d7ae4939a71ab0cc2
Jaygithubs <jaysharma89533@gmail.com>
2024-06-17 21:29:15 -1200

    connnect db code

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,3 @@
-// import express from "express"
-// import mongoose from "mongoose"
-// import dotenv from "dotenv"
-
-
-// const app=express();
-// dotenv.config();
-
-// const PORT = process.env.PORT || 7000;
-// const MONGOURL = process.env.MONGO_URL;
-
-// mongoose.connect("MONGOURL").then(()=>{
-//     console.log("Database connected succesfully");
-//     app.listen(PORT,()=>{
-//         console.log(`server is running on ${PORT}`);
-//     });
-// }).catch((error)=>console.log(error));
-
 const mongoose = require('mongoose');
 
 const uri = 'mongodb+srv://jaysharma89533:%23jayDB@jaymongodb.fdlxsvh.mongodb.net/';
